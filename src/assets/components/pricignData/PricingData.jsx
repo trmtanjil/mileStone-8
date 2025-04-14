73b2d758721ed0e0e438471c5176ecfe68f02bc5
Tanjilur Rahman <trmtanjil02@gmail.com>
2025-04-14 06:21:45 +0600

    daisy ui modify responsive pricing cart compleat commit

--- src/assets/components/pricignData/PricingData.jsx
+++ src/assets/components/pricignData/PricingData.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import PricingCart from './PricingCart';
+import DaysiPricingCart from '../daysiPricingCart/daysiPricingCart';
 
 const PricingData = ({pricingFechData}) => {
 
@@ -11,12 +12,21 @@ const pricinguse = use(pricingFechData)
 
             <h2>Get out member ship </h2>
             <div className='grid md:grid-cols-3 gap-4's>
-                {
+                {/* {
                     pricinguse.map(pricing=>
                         <PricingCart
                         key={pricing.id}
                         pricing={pricing}
                         ></PricingCart>
+                    )
+                } */}
+
+                {
+                    pricinguse.map(pricing =>
+                        <DaysiPricingCart
+                        key={pricing.id}
+                        pricing={pricing}
+                        ></DaysiPricingCart>
                     )
                 }
             </div>
